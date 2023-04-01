f5f5880381093089c611253e62ee40eb3d2edc16
Filipe <filipevp1999@hotmail.com>
2023-03-31 22:03:54 -0300

    fix rollback

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,10 @@ export default function CharactersList() {
   if (isLoading) {
     return (
       <PageContainer >
+        <div className="topbar-container">
+          <SearchFilter setData={setFilteredList} action={filterCharacter} refetch={refetch} />
+          <Paginator pagesNumber={pagesNumber} page={page} setPage={setPage} />
+        </div>
         <CharactersContainer >
           {
             Array.from(Array(20).keys()).map((x,index)=>{
